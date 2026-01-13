4606aa619368f0b0afaf5d3df602d2128922c20a
AbhisekDas01 <dasabhisek668@gmail.com>
2026-01-13 22:47:24 +0530

    created the authCaptain middleware

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import userModel from "../models/user.model.js";
 import jwt from 'jsonwebtoken';
 import { ENV_JWT_SECRET } from "../configs/env.config.js";
 import blacklistTokenModel from "../models/blacklistToken.model.js";
+import captianModel from "../models/captain.model.js";
 
 export const authUser = async (req, res, next) => {
 
@@ -40,5 +41,44 @@ export const authUser = async (req, res, next) => {
         return res.status(401).json({
             message: 'Unauthorized'
         });
+    }
+}
+
+export const authCaptain = async (req , res , next ) => {
+
+
+    const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    const isBlackListed = await blacklistTokenModel.findOne({token});
+
+    if(isBlackListed) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
+    }
+
+    try {
+
+        const decoded = jwt.verify(token , ENV_JWT_SECRET);
+
+        const captain = await captianModel.findById(decoded._id);
+
+        if (!captain) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        req.captain = captain;
+        return next();
+        
+    } catch (error) {
+        return res.status(401).json({
+            message: "Unauthorized!"
+        })
     }
 }
