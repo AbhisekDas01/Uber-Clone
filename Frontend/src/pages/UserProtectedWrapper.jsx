8938023743937ccda7bfdc5f6611c171147e5692
AbhisekDas01 <dasabhisek668@gmail.com>
2026-01-17 23:51:32 +0530

    add authorization route -> captain wrapper -> user wrapper -> they call to for the user / captain data in every reload

--- Frontend/src/pages/UserProtectedWrapper.jsx
+++ Frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,20 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import { UserDataContext } from '../context/UserContext';
+import axios from 'axios';
+import Loading from '../components/Loading';
 
 // Redirect unauthenticated users away from protected pages.
 const UserProtectedWrapper = ({ children }) => {
     const token = localStorage.getItem('token');
     const navigate = useNavigate();
+    const {setUser} = useContext(UserDataContext);
+    const [loading , setLoading] = useState(true);
 
     useEffect(() => {
         if (!token) {
-            toast.error("Please login first!");
-            navigate('/login', { replace: true });
+            navigate('/login');
         }
-    }, [token, navigate]);
 
-    if (!token) return null;
+        axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setUser(response.data.user)
+                setLoading(false)
+            }
+        })
+            .catch(err => {
+                localStorage.removeItem('token')
+                navigate('/login')
+            })
+    }, [ token ])
+
+    if (loading) {
+        return (
+           <Loading/>
+        )
+    }
+
 
     return <>{children}</>;
 }
