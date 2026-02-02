e782949a24b985446bca77800a2833826e952b29
AbhisekDas01 <dasabhisek668@gmail.com>
2026-02-02 23:18:53 +0530

    created websocket connection with socket.io

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 import http from 'node:http';
 import app from './app.js';
 import { ENV_PORT } from './configs/env.config.js';
+import { initializeSocket } from './socket.js';
 
 const server = http.createServer(app);
+
+initializeSocket(server);
 
 server.listen(ENV_PORT || 3000, () => {
     console.log(`Server is running on port ${ENV_PORT || 3000}`);
